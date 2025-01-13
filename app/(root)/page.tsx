c60471ff818be2fca30952f285985e22e8b7fdc6
Yirga M. Beyene <infoyirga12@gmail.com>
2025-01-13 20:33:49 +0300

    Implement root layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { Button } from "@/components/ui/button";
+
+const Home = () => (
+  <>
+    <BookOverview />
+    <BookList />
+  </>
+);
+
+export default Home;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-const Home = () => (
-  <>
-    <Button variant="default">Click me</Button>
-  </>
-);
-
-export default Home;
