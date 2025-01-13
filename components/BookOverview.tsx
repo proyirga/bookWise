271d51c58c6061542dd921cb386769c7a6b1316b
Yirga M. Beyene <infoyirga12@gmail.com>
2025-01-13 22:23:43 +0300

    Implement Hero Section with BookCoverSvg componenet

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,10 +1,68 @@
+import Image from "next/image";
 import React from "react";
+import { Button } from "./ui/button";
+import BookCover from "./BookCover";
 
-const BookOverview = () => {
+const BookOverview = ({
+  title,
+  author,
+  genre,
+  rating,
+  totalCopies,
+  availableCopies,
+  description,
+  color,
+  coverUrl,
+}: Book) => {
   return (
     <section className="book-overview">
-      <div className="flex flex-1 gap-5">
-        <h1>A Long Book Title</h1>
+      <div className="flex flex-col gap-5">
+        <h1>{title}</h1>
+        <div className="flex flex-col gap-5">
+          <div className="book-info">
+            <p>
+              By <span className="font-semibold text-light-200">{author}</span>
+            </p>
+            <p>
+              Category{" "}
+              <span className="font-semibold text-light-200">{genre}</span>
+            </p>
+            <div className="flex flex-row gap-1">
+              <Image src="/icons/star.svg" width={22} height={22} alt="star" />
+              <p>{rating}</p>
+            </div>
+          </div>
+          <div className="book-copies">
+            <p>
+              Total Books: <span>{totalCopies}</span>
+            </p>
+            <p>
+              Available Books: <span>{availableCopies}</span>
+            </p>
+          </div>
+          <p className="book-description">{description}</p>
+          <Button className="book-overview-btn">
+            <Image src="/icons/book.svg" width={20} height={20} alt="book" />
+            <p className="font-bebas-neue text-xl text-dark-100">Borrow</p>
+          </Button>
+        </div>
+      </div>
+      <div className="relative flex flex-1 justify-center">
+        <div className="relative">
+          <BookCover
+            variant="wide"
+            className="z-10"
+            coverColor={color}
+            coverImage={coverUrl}
+          />
+          <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
+            <BookCover
+              variant="wide"
+              coverColor={color}
+              coverImage={coverUrl}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
