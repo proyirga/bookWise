3fa4e7d0f19ff56cd3d5451ffa431828d933c161
Yirga M. Beyene <infoyirga12@gmail.com>
2025-01-15 11:12:16 +0300

    Setup database

---
+++ database/schema.ts
@@ -0,0 +1,32 @@
+import {
+  uuid,
+  varchar,
+  integer,
+  text,
+  date,
+  timestamp,
+  pgEnum,
+  boolean,
+  pgTable,
+} from "drizzle-orm/pg-core";
+
+export const STATUS_ENUM = pgEnum("status", [
+  "PENDING",
+  "APPROVED",
+  "REJECTED",
+]);
+export const ROLE_ENUM = pgEnum("role", ["USER", "ADMIN", "SUPER_ADMIN"]);
+export const BOOK_STATUS_ENUM = pgEnum("book_status", ["BORROWED", "RETURNED"]);
+
+export const users = pgTable("users", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  fullName: varchar("full_name", { length: 255 }).notNull(),
+  email: text("email").notNull().unique(),
+  universityId: integer("university_id").notNull().unique(),
+  password: text("password").notNull(),
+  universityCard: text("university_card").notNull().unique(),
+  status: STATUS_ENUM("status").default("PENDING"),
+  role: ROLE_ENUM("role").default("USER"),
+  lastActivityDate: date("last_activity_date").defaultNow(),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+});
